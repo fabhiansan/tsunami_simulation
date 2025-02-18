b54451b6176f127ca83db5ad7ac3488aa2cd5d42
bragapm <braga.productmanager@gmail.com>
2025-02-18 13:03:10 +0700

    Add reprojected road boundaries and pedestrian road data for Pacitan region
    - Imported two new ASC raster files for road-related geospatial data
    - Files include road boundaries and pedestrian road information
    - Geospatial data uses consistent projection and 2m cell resolution
    - Covers the Pacitan region with 4324 columns and 4332 rows

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,4 +1,30 @@
-pub mod game;
-pub mod agent;
+use rand::seq::SliceRandom; // Impor trait SliceRandom
+
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
+#[derive(Eq, PartialEq)]
+pub struct State {
+    cost: u32,
+    x: u32,
+    y: u32,
+}
+
+// Implementasi Ord dan PartialOrd untuk membuat BinaryHeap berperilaku sebagai min-heap.
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Kita membalik perbandingan sehingga elemen dengan cost rendah memiliki prioritas tinggi.
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
 pub mod grid;
-pub mod camera;
+pub mod agent;
+pub mod game;
